54e26a91c6be52c1563d5ed7506e2043646b478e
Shlomi Goland <golandsh@gmail.com>
2019-01-23 19:24:18 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'yulia@example.com',
-    text: 'Sure, meet me at 6.'
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'shlomi@example.com',
-    text: 'Hi, meet me at the movies',
-    createdAt: 123
-  });
-
   socket.on('createMessage', (newMessage) => {
     console.log('Create message', newMessage);
+
+    io.emit('newMessage', {
+      from: newMessage.from,
+      text: newMessage.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
